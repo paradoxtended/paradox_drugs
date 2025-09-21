cb39bde912473668e7b0b782495d0c1b05d5912e
Timothy Barabas <215121906+im-ppaull@users.noreply.github.com>
2025-09-21 13:39:08 +0200

    Migrate web client from React to Svelte
    Replaces the React-based web client with a new Svelte setup. Adds Svelte, Tailwind, and related config files, restructures hooks and utils, removes React components and hooks, and updates project configuration files to support the new stack.

---
+++ web/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { isEnvBrowser } from "./lib/utils/misc";
+
+if (isEnvBrowser()) {
+  const root = document.getElementById('app');
+
+  // https://i.imgur.com/iPTAdYV.png - Night time img
+  root!.style.backgroundImage = 'url("https://i.imgur.com/3pzRj9n.png")';
+  root!.style.backgroundSize = 'cover';
+  root!.style.backgroundRepeat = 'no-repeat';
+  root!.style.backgroundPosition = 'center';
+}
+</script>
+
+<div>
+    CERT TO ZOBER NEVADI AJ TAK SI S VAMI DAM
+</div>
